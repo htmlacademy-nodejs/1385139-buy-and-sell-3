526f4808719a446090aa22d5eaec68789126414a
Anastasia <ana.izotova@gmail.com>
2021-07-02 18:35:04 +0300

    add more titles & descriptions, fix eslint errors

--- src/service/cli/generate.js
+++ src/service/cli/generate.js
@@ -14,6 +14,10 @@ const TITLES = [
   `Продам отличную подборку фильмов на VHS`,
   `Куплю антиквариат`,
   `Куплю породистого кота`,
+  `Продам коллекцию журналов «Огонёк»`,
+  `Отдам в хорошие руки подшивку «Мурзилка»`,
+  `Продам советскую посуду. Почти не разбита`,
+  `Куплю детские санки`
 ];
 
 const SENTENCES = [
@@ -27,6 +31,10 @@ const SENTENCES = [
   `Если найдёте дешевле — сброшу цену.`,
   `Таких предложений больше нет!`,
   `При покупке с меня бесплатная доставка в черте города.`,
+  `Кажется, что это хрупкая вещь`,
+  `Мой дед не мог её сломать`,
+  `Кому нужен этот новый телефон, если тут такое...`,
+  `Не пытайтесь торговаться. Цену вещам я знаю`
 ];
 
 const CATEGORIES = [
@@ -52,24 +60,24 @@ const SumRestrict = {
 const PictureRestrict = {
   MIN: 1,
   MAX: 16
-}
+};
 
 const getPictureFileName = (number) => number > 10 ? `item${number}.jpg` : `item0${number}.jpg`;
 
 const generateOffers = (count) => (
   Array(count).fill({}).map(() => ({
-    category: [CATEGORIES[getRandomInt(0, CATEGORIES.length - 1)]],
-    description: shuffle(SENTENCES).slice(1, 5).join(` `),
-    picture: getPictureFileName(getRandomInt(PictureRestrict.MIN, PictureRestrict.MAX)),
     title: TITLES[getRandomInt(0, TITLES.length - 1)],
+    picture: getPictureFileName(getRandomInt(PictureRestrict.MIN, PictureRestrict.MAX)),
+    description: shuffle(SENTENCES).slice(1, 5).join(` `),
     type: OfferType[Object.keys(OfferType)[Math.floor(Math.random() * Object.keys(OfferType).length)]],
     sum: getRandomInt(SumRestrict.MIN, SumRestrict.MAX),
+    category: [CATEGORIES[getRandomInt(0, CATEGORIES.length - 1)]],
   }))
 );
 
 module.exports = {
   name: `--generate`,
-  run() {
+  run(args) {
     const [count] = args;
     const countOffer = Number.parseInt(count, 10) || DEFAULT_COUNT;
     const content = JSON.stringify(generateOffers(countOffer));
