52c597b6b083092e7d2022746881e55db57801a0
Anastasia <ana.izotova@gmail.com>
2021-09-05 11:36:54 +0300

    refactor func using async/await

--- src/service/cli/generate.js
+++ src/service/cli/generate.js
@@ -1,11 +1,11 @@
 'use strict';
-const fs = require(`fs`);
+const fs = require(`fs`).promises;
+const chalk = require(`chalk`);
 const {
   getRandomInt,
   shuffle,
 } = require(`../../utils`);
 const {ExitCode} = require(`../../const`);
-const chalk = require("chalk");
 
 const DEFAULT_COUNT = 1;
 const MAX_OFFERS_COUNT = 1000;
@@ -82,7 +82,7 @@ const generateOffers = (count) => (
 
 module.exports = {
   name: `--generate`,
-  run(args) {
+  async run(args) {
     const [count] = args;
     const countOffer = Number.parseInt(count, 10) || DEFAULT_COUNT;
 
@@ -93,12 +93,11 @@ module.exports = {
 
     const content = JSON.stringify(generateOffers(countOffer));
 
-    fs.writeFile(FILE_NAME, content, (err) => {
-      if (err) {
-        return console.error(chalk.red(`Can't write data to file...`));
-      }
-
-      return console.info(chalk.green(`Operation success. File created.`));
-    });
+    try {
+      await (fs.writeFile(FILE_NAME, content));
+      console.info(chalk.green(`Operation success. File created.`));
+    } catch (err) {
+      console.error(chalk.red(`Can't write data to file...`));
+    }
   }
 };
