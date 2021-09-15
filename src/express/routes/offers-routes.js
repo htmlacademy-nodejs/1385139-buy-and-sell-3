5a81cb95b281bbfe591e096fc3b9718e0265a475
Anastasia <izotova@sminex.com>
2021-09-15 18:13:39 +0300

    fix eslint errors

--- src/express/routes/offers-routes.js
+++ src/express/routes/offers-routes.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const {Router} = require(`express`);
-const offersRouter  = new Router();
+const offersRouter = new Router();
 
 offersRouter.get(`/category/:id`, (req, res) => res.send(`/offers/category/:id`));
 offersRouter.get(`/add`, (req, res) => res.send(`/offers/add`));
